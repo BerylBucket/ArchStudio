cea7b9112ceb28240f1db3af579195744efbf1b1
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 11:17:57 -0700

    Update Home.vue

--- src/Home.vue
+++ src/Home.vue
@@ -5,11 +5,13 @@
 Welcome to Arch Studio
     </template>
     <template v-slot:body>
+    <div>
       <p>We have a unique network and skillset to help bring your projects to life. Our small team of highly skilled individuals combined with our large network put us in a strong position to deliver exceptional results. </p>
       <p>
       Over the past 10 years, we have worked on all kinds of projects. From stations to high-rise buildings, we create spaces that inspire and delight.</p>
       <p>We work closely with our clients so that we understand the intricacies of each project. This allows us to work in harmony the surrounding area to create truly stunning projects that will stand the test of time.</p>
-    </template>
+   </div>
+ </template>
   </sect>
 </template>
 
