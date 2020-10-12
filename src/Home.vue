76603f163ba888d91f118195a7d6a3ad75331d5f
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 16:54:46 -0700

    Update Home.vue

--- src/Home.vue
+++ src/Home.vue
@@ -11,19 +11,39 @@
         <p>We work closely with our clients so that we understand the intricacies of each project. This allows us to work in harmony the surrounding area to create truly stunning projects that will stand the test of time.</p>
       </template>
     </Sect>
+    <section id="featured">
+    <h3>Featured</h3>
+    <div id="featImgs">
+      <featimg img="https://i.ibb.co/HKTjKck/FA26-FC69-1523-4-E38-97-EA-47-C218-EE375-B.jpg" num="1">Project Del Sol</featimg>
+      <featimg img="https://i.ibb.co/7gT2J5b/D4-A03-BE9-175-E-47-BF-89-E2-6-F80-A9-A26407.jpg" num="2">228B Tower</featimg>
+      <featimg img="https://i.ibb.co/T08S8tp/49-D8-D034-211-C-4-DE4-AD62-FCB6-D596-CE0-E.jpg" num="3">Le Prototype</featimg>
+    </div>
+  </section>
     <Foot></Foot>
   </div>
 </template>
+
+<style>
+  #featured{
+    padding: 2rem;
+  }
+  #featImgs{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-gap: 1rem;
+  }
+</style>
 
 <script>
   import Ln from "./components/Ln";
   import Carousel from "./components/Carousel";
   import Sect from "./components/Sect";
+  import FeatImg from "./components/img/FeatImg";
   import Foot from "./components/Foot";
   export default {
     name: "Home",
     components: {
-      Ln, Sect, Foot, Carousel
+      Ln, Sect, Foot, Carousel, FeatImg
     }
   }
 </script>
