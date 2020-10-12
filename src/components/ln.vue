62ca3654b42e06271b74c73e694f678cabfadcec
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 10:43:05 -0700

    Create ln.vue

---
+++ src/components/ln.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="line"></div>
+</template>
+
+<script>
+  export default {
+   name: "ln"
+}
+</script>
+
+<style scoped>
+  .line{border-bottom: #ccc solid 1px;width: 6rem; margin-bottom: 4rem;}
+</style>
