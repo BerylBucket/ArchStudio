99d3618a2c7986a1c5e630f8d87071766938517a
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 15:42:39 -0700

    Update Sect.vue

--- src/components/Sect.vue
+++ src/components/Sect.vue
@@ -29,6 +29,28 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+  font-family: 'Spartan';
+  font-style: normal;
+  font-weight: 500;
+  src: url(https://fonts.gstatic.com/s/spartan/v2/l7gfbjR61M69yt8Z2QKtlAXJExs.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+@font-face {
+  font-family: 'Spartan';
+  font-style: normal;
+  font-weight: 700;
+  src: url(https://fonts.gstatic.com/s/spartan/v2/l7gfbjR61M69yt8Z2QKtlAXJExs.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+  font-family: Spartan, sans-serif;
+}
+
   .sect {
     padding: 2rem;
     display: grid;
