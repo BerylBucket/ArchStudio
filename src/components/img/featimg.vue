126422b10b0c546894deee1d63fd24f38795a53e
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 11:35:25 -0700

    Create featimg.vue

---
+++ src/components/img/featimg.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="featImg"><portimg :imgurl="img"><template v-slot:name><slot></slot></template><template v-slot:date><a>View All Projects</a></template></portimg><div class="num">{{num}}</div></div>
+</template>
+
+<script>
+  export default {
+    name: "featimg",
+    props: {
+      img: String,
+      num: String
+    }
+  }
+</script>
+
+<style scoped>
+  .featImg{position:relative;}.featImg .num {font-weight: 700;position: absolute; top: 18px;right:0;font-size: 100px;color: rgba(255,255,255,.3);}
+</style>
