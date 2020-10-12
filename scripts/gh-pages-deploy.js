67d7fe999a3d595e055a34b30d0e0f8481b16245
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 10:34:32 -0700

    Update gh-pages-deploy.js

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,18 +3,18 @@ const execa = require("execa");
 
   (async () => {
     try {
-      await execa("git", ["checkout", "--orphan", "master"]);
+      await execa("git", ["checkout", "--orphan", "main"]);
       console.log("Building...");
       await execa("npm", ["run", "build"]);
       // Understand if it's dist or build folder
       const folderName = fs.existsSync("dist") ? "dist" : "build";
       await execa("git", ["--work-tree", folderName, "add", "--all"]);
-      await execa("git", ["--work-tree", folderName, "commit", "-m", "master"]);
+      await execa("git", ["--work-tree", folderName, "commit", "-m", "main"]);
       console.log("Pushing to gh-pages...");
       await execa("git", ["push", "origin", "HEAD:master", "--force"]);
       await execa("rm", ["-r", folderName]);
       await execa("git", ["checkout", "-f", "dev"]);
-      await execa("git", ["branch", "-D", "master"]);
+      await execa("git", ["branch", "-D", "main"]);
       console.log("Successfully deployed");
     } catch (e) {
       console.log(e.message);
