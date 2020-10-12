7c207eddb159ccbdeea67f04dd2482d09ba01407
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 11:00:41 -0700

    Create sect.vue

---
+++ src/components/sect.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="sect">
+  <div class="body">
+    <h1>Welcome</h1>
+    <div class="spacer"></div>
+    <div class="lineWrap">
+      <ln></ln>
+    </div>
+<h2><slot name="heading"></slot></h2>
+<slot name="body"></slot>
+</div>
+  <div :style="'background-image: url(' + this.img + ')'" class="image"> </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "sect",
+  props: {
+    img: String
+  }
+};
+</script>
+
+<style scoped>
+  .sect {
+  padding: 2rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10%, 1fr));
+  grid-gap: 1.5rem;
+}
+@media screen and (min-width: 1024px) {
+  .sect {
+    padding: 5rem;
+  }
+  .sect .lineWrap {
+    opacity: 0;
+  }
+}
+@media screen and (max-width: 1023px) {
+  .sect .spacer, .sect h1{
+    display: none;
+  }
+}
+.sect .spacer{
+  height: 98px;
+}
+.sect h1{
+  position: absolute;
+  top:0;
+  left:0;
+  font-size: 160px;
+  color: #EEEFF4;
+}
+.sect h2 {
+  font-size: 2.5rem;
+}
+.sect p {
+  color: #60636d;
+  margin-top: 2rem;
+}
+.sect .body {
+  grid-column: span 5;
+  position: relative;
+}
+.sect .image {
+  width: auto;
+  height: 100%;
+  float: right;
+  grid-column: span 3;
+background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
