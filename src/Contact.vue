8a30ceb0c5d8decd40cccb2e4c3a5d3853710a8e
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 17:28:24 -0700

    Create Contact.vue

---
+++ src/Contact.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <Foot></Foot>
+  </div>
+</template>
+
+<script>
+  import Navbar from "./components/Navbar";
+  import Foot from "./components/Foot";
+</script>
