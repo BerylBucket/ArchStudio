558b7de86f713fcc594811a131628ee6befdb10f
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 18:53:44 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
  rules: {
+     "no-console": 0,
+     "quotes": [2, "double", "avoid-escape"],
     'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }]
-  }
+  },
+ parser: "vue-eslint-parser"
 }
