8ea187549fd1593bf40efb2e253446cbacaafa2b
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 19:04:51 -0700

    Update Foot.vue

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -9,5 +9,15 @@
 </script>
 
 <style scoped>
-  .footer {background: linear-gradient(to left, transparent 100px, #eee 100px);height: 120px;color: #777;position: relative; margin-top:20px;}.footer .logo { display: inline-block; font-weight: 900;font-size: 36px;letter-spacing: -0.15em; background: #222; color: #f7f7f7; margin-right: 60px;width: 120px;height: 120px;line-height: 120px;text-align: center;}.footer .link {display: inline-block;transform: translateY(-8px); padding: 18px 20px; text-decoration: none;}.footer .btn {display:flex;font-weight: 700;position: absolute;right: 0;bottom: calc(50% - 26px);background: #222;color: #f7f7f7;padding: 16px;}@media (max-width: 700px) {.footer {height: 420px;text-align: center;background: linear-gradient(to bottom, transparent 60px, #eee 60px);}.footer .logo {display: inline-block;margin: 0 auto 30px auto;}.footer .link {background: #eee;float: none;display: block;color: #777;width: 100%;}.footer .btn {display: inline-block; position: relative;margin-top: 12px;}.footer .btn svg{transform: translateY(3px);}}
+  .footer {background: linear-gradient(to left, transparent 100px, #eee 100px);height: 120px;color: #777;position: relative; margin-top:30px;}
+  .footer .logo { display: inline-block; font-weight: 900;font-size: 36px;letter-spacing: -0.15em; background: #222; color: #f7f7f7; margin-right: 60px;width: 120px;height: 120px;line-height: 120px;text-align: center;}
+  .footer .link {display: inline-block;transform: translateY(-8px); padding: 18px 20px; text-decoration: none;}
+  .footer .btn {display:flex;font-weight: 700;position: absolute;right: 0;bottom: calc(50% - 26px);background: #222;color: #f7f7f7;padding: 16px;}
+  @media (max-width: 700px) {
+  .footer {height: 420px;text-align: center;background: linear-gradient(to bottom, transparent 60px, #eee 60px);}
+  .footer .logo {display: inline-block;margin: 0 auto 30px auto;}
+  .footer .link {background: #eee;float: none;display: block;color: #777;width: 100%;}
+  .footer .btn {display: inline-block; position: relative;margin-top: 12px;}
+  .footer .btn svg{transform: translateY(3px);}
+  }
 </style>
