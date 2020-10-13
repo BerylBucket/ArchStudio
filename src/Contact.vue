1999c514be3c094c02286ad0a96db94e22508e24
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-12 18:00:37 -0700

    Update Contact.vue

--- src/Contact.vue
+++ src/Contact.vue
@@ -6,6 +6,12 @@
       <template v-slot:heading>Tell us about your project</template>
       <template v-slot:body>We’d love to hear more about your project. Please, leave a message below or give us a call. We have two offices, one in Texas and one in Tennessee. If you find yourself nearby, come say hello!</template>
     </Intro>
+    <section id="contactForm">
+    <h2>Connect with us</h2>
+    <ArchForm>
+      <input placeholder="Name"></input><input placeholder="Email"></input><textarea placeholder="Message"></textarea>
+    </ArchForm>
+  </section>
     <Foot></Foot>
   </div>
 </template>
@@ -13,11 +19,12 @@
 <script>
   import Navbar from "./components/Navbar";
   import Intro from "./components/Intro";
+  import ArchForm from "./components/ArchForm";
   import Foot from "./components/Foot";
   
   export default {
     components: {
-      Navbar, Intro, Foot
+      Navbar, Intro, ArchForm, Foot
     }
   }
 </script>
