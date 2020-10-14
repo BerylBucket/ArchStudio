d9631b840f0e0a1e69bbbb3c6a5430a69de7c3c5
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-13 19:25:48 -0700

    Update contact.js

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import Ho from "./Ho";
+import Contact from "./Contact";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(Ho)
+  render: h => h(Contact)
 }).$mount("#app");
